e7108d458d225ed7add4c2db5de092fbfac25cd9
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2022-01-01 20:41:14 +0300

    feat: two-to-one

---
+++ two-to-one.js
@@ -0,0 +1,39 @@
+/* 
+
+Description:
+Take 2 strings s1 and s2 including only letters from ato z.
+Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+ */
+
+function longest(s1, s2) {
+  let result = [];
+  let concatStr = s1 + s2;
+  
+  for (let i = 0; i < concatStr.length; i++) {
+    if (result.includes(concatStr[i])) {
+      continue;
+    } 
+    result.push(concatStr[i]);
+  }
+  
+  result.sort(function(a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+    return 0;
+  });
+  
+  return result.join("");
+}
